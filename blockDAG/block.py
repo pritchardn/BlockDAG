621cac04f06e088674e6e0ed733f9be200804056
pritchardn <21726929@student.uwa.edu.au>
2020-04-15 11:57:52 +0800

    Basic implementation of block with handlers for testing written

---
+++ blockDAG/block.py
@@ -0,0 +1,92 @@
+# Author: Nic Pritchard
+import enum
+import hashlib
+import json
+from dataclasses import dataclass, asdict
+from enforce_typing import enforce_types
+
+
+class Goal(enum.Enum):
+    Rerun = 0
+    Repeat = 1
+    Reproduce = 2
+    Replicate = 3
+
+
+class PayloadType(enum.Enum):
+    Genesis = 0
+    Code = 1
+    Data = 2
+    Meta = 3
+
+
+@enforce_types
+@dataclass
+class Header:
+    numparents: int
+    parents: list
+    payloadhash: bytes  # Generated at creation
+    blockhash: bytes  # Hashing payloadhash and parent(s) together
+
+
+@enforce_types
+@dataclass
+class GenesisPayload:
+    pass
+
+
+@enforce_types
+@dataclass
+class CodePayload:
+    language: str
+    runtime: str
+    code: str
+    versiontag: str
+
+
+@enforce_types
+@dataclass
+class DataPayload:
+    length: int
+    data: bytes
+
+
+@enforce_types
+@dataclass
+class MetaPayload:
+    authors: str
+    intitute: str
+    goal: Goal
+
+
+class Block(object):
+
+    def __init__(self, numparents: int, parents: list, payload):
+        if type(numparents) != int:
+            raise TypeError("numparents must be int")
+        if type(parents) != list:
+            raise TypeError("parents needs to be a list of bytes objects")
+
+        if type(payload) == GenesisPayload:
+            self.ptype = PayloadType.Genesis
+            if numparents != 0:
+                raise ValueError("Genesis block must have no parents")
+            if parents:
+                raise ValueError("Genesis block must have no parents []")
+        elif numparents <= 0:
+            raise ValueError("Positive number of parents needed")
+        else:
+            all(isinstance(i, bytes) for i in parents)
+        if type(payload) == CodePayload:
+            self.ptype = PayloadType.Code
+        elif type(payload) == DataPayload:
+            self.ptype = PayloadType.Data
+        elif type(payload) == MetaPayload:
+            self.ptype = PayloadType.Meta
+
+        payhash = hashlib.sha3_256(json.dumps(asdict(payload), sort_keys=True).encode('utf-8')).digest()
+        headhash = hashlib.sha3_256(b''.join(parents) + payhash).digest()
+        self.header = Header(numparents, parents, payhash, headhash)
+        # print(self.header.payloadhash)
+        # print(self.header.blockhash)
+        # print("-------- == --------")
---
+++ test/test_block.py
@@ -0,0 +1,22 @@
+from pytest import raises
+from block import *
+
+
+def test_block_type_errors():
+    assert True
+
+
+def test_block_genesis():
+    assert True
+
+
+def test_block_code():
+    assert True
+
+
+def test_block_data():
+    assert True
+
+
+def test_block_meta():
+    assert True
