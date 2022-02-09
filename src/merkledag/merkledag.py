826e4410cee4f22a1ed33a90404ae2a80e4edff0
Nicholas Pritchard <21726929@student.uwa.edu.au>
2022-02-09 13:42:56 +0800

    Adds basic API

--- src/merkledag/merkledag.py
+++ src/merkledag/merkledag.py
@@ -1,2 +1,11 @@
-class MerkleDAG:
+def build_merkle_dag(vertices, edges, hash_function, data_fields, append_global=True, append_hash=True, overwrite=True):
+    pass
+
+def compare_dags(vertices_1, edges_1, vertices_2, edgeS_2):
+    pass
+
+def verify_leaf_inclusion(vertices, edges):
+    pass
+
+def pretty_print(vertices, edges):
     pass
