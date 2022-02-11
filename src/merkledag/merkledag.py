f3ca47a82fa673d3ef540db319123bccf84c456a
pritchardn <21726929@student.uwa.edu.au>
2022-02-11 15:45:27 +0800

    Cleanup and implementation of _build_hash_payload

--- src/merkledag/merkledag.py
+++ src/merkledag/merkledag.py
@@ -1,9 +1,18 @@
 import collections
+from merklelib import MerkleTree
 
-def _build_hash_payload(vertex: dict, data_fields):
-    return {'data_hash': b''}
 
-def build_merkle_dag(vertices: dict, edges: dict, hash_function: function, data_fields, append_global=True, append_hash=True, overwrite=True):
+def _build_hash_payload(vertex: dict, data_fields, hash_function):
+    data = []
+    for key, val in vertex.items():
+        if key in data_fields:
+            data.append(val)
+    mtree = MerkleTree(sorted(data), hash_function)
+    return {'data_hash': mtree.merkle_root}
+
+
+def build_merkle_dag(vertices: dict, edges: dict, hash_function, data_fields, append_global=True,
+                     append_hash=True, overwrite=True):
     dropset = {}
     outputset = {}
     neighbourset = {}
@@ -30,7 +39,7 @@ def build_merkle_dag(vertices: dict, edges: dict, hash_function: function, data_
 
     while queue:
         id = queue.pop()
-        outputset[id][0] = _build_hash_payload(dropset[id][0], data_fields)
+        outputset[id][0] = _build_hash_payload(dropset[id][0], data_fields, hash_function)
         visited.append(id)
         for neighbour in neighbourset[id]:
             current_neighbour = dropset[neighbour][0]
@@ -49,8 +58,10 @@ def build_merkle_dag(vertices: dict, edges: dict, hash_function: function, data_
 def compare_dags(vertices_1, edges_1, vertices_2, edgeS_2):
     pass
 
+
 def verify_leaf_inclusion(vertices, edges):
     pass
+
 
 def pretty_print(vertices, edges):
     pass
