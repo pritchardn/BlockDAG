9afbd8b80900550ed027079fda0918090164c489
pritchardn <21726929@student.uwa.edu.au>
2022-08-10 16:07:11 +0800

    Bug-fixes in compare_dags: checks for hasattr("__getitem__"), avoids processing signature entry

--- src/blockdag/blockdag.py
+++ src/blockdag/blockdag.py
@@ -184,14 +184,18 @@ def compare_dags(vertices_1: dict, vertices_2: dict):
     sigmap_1 = {}
     sigmap_2 = {}
     for key, val in vertices_1.items():
-        if not hasattr(val, "__get__"):
+        if key == "signature":
+            continue
+        if not hasattr(val, "__getitem__"):
             continue
         if val.get("hash") is not None:
             sigmap_1[val["hash"]] = key
         else:
             raise ValueError(f"Vertex {key} does not contain hash")
     for key, val in vertices_2.items():
-        if not hasattr(val, "__get__"):
+        if key == "signature":
+            continue
+        if not hasattr(val, "__getitem__"):
             continue
         if val.get("hash") is not None:
             sigmap_2[val["hash"]] = key
